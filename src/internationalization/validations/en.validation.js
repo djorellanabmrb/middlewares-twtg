9a454375afd15a3e2f32e9c340115d98ff4ade44
diego jose orellana bojorquez <diegoorellana@twowheelstogo.com>
2020-09-22 10:03:28 -0600

    add more validations

--- src/internationalization/validations/en.validation.js
+++ src/internationalization/validations/en.validation.js
@@ -1,4 +1,7 @@
 module.exports = {
     "val1":"page must be numeric",
-    "val2":"name must be string"
+    "val2":"name must be string",
+    "val3":"countryId must be integer",
+    "val4":"regionId must be integer",
+    "val5":"cityId must be integer"
 };
--- src/internationalization/validations/es.validation.js
+++ src/internationalization/validations/es.validation.js
@@ -1,4 +1,7 @@
 module.exports = {
     "val1":"la página debe ser númerico",
-    "val2":"el nombre de la búsqueda debe ser cadena"
+    "val2":"el nombre de la búsqueda debe ser cadena",
+    "val3":"el countryId debe ser entero",
+    "val4":"el regionId debe ser entero",
+    "val5":"el cityId debe ser entero"
 };

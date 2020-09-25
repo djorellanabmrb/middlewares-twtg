5e4a1579a9f99f7eaf7951c253de814002259677
diego jose orellana bojorquez <diegoorellana@twowheelstogo.com>
2020-09-25 09:28:23 -0600

    add validations of latitude and longitude

--- src/internationalization/validations/en.validation.js
+++ src/internationalization/validations/en.validation.js
@@ -3,5 +3,7 @@ module.exports = {
     "val2":"name must be string",
     "val3":"countryId must be integer",
     "val4":"regionId must be integer",
-    "val5":"cityId must be integer"
+    "val5":"cityId must be integer",
+    "val6":"latitude must be float between -90 and 90",
+    "val7":"longitude must be float between -180 and 180"
 };
--- src/internationalization/validations/es.validation.js
+++ src/internationalization/validations/es.validation.js
@@ -3,5 +3,7 @@ module.exports = {
     "val2":"el nombre de la búsqueda debe ser cadena",
     "val3":"el countryId debe ser entero",
     "val4":"el regionId debe ser entero",
-    "val5":"el cityId debe ser entero"
+    "val5":"el cityId debe ser entero",
+    "val6":"la latitud debe ser un número decimal entre -90 a 90.",
+    "val7":"la longiut debe ser un número decimal entre -180 a 180"
 };

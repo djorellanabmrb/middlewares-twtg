f18dbce84767bc82545298509fc1db43a6192532
diego jose orellana bojorquez <diegoorellana@twowheelstogo.com>
2020-10-17 06:28:29 -0600

    fix companies

--- src/internationalization/errors/companies/es.error.company.js
+++ src/internationalization/errors/companies/es.error.company.js
@@ -1,3 +1,4 @@
 module.exports = {
-    "e1":"Algún identificador de país, región, ciudad o pueblo está incorrecto"
+    "e1":"Algún identificador de país, región, ciudad o pueblo está incorrecto",
+    "e2":"No se ha encontradon ningún lugar transitado",
 }

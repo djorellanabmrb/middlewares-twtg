51bdb6ef95935adbcd73b29c198086d95df48c84
diego jose orellana bojorquez <diegoorellana@twowheelstogo.com>
2020-10-21 09:21:01 -0600

    pass parameters internationalization and jwt

--- src/internationalization.js
+++ src/internationalization.js
@@ -1,25 +1,26 @@
 const Polyglot = require('node-polyglot');
 const {LanguageEnum} = require("./enums");
-const Internationalization = require("./internationalization/index");
 
 class InternationalizationMiddleware{
     constructor(){
     }
-    async attachI18(req, res, next){
-        let acceptLanguage = req.header('Accept-Language');
-        try {
-            LanguageEnum.isValidEnum(acceptLanguage);
-        } catch (error) {
-            acceptLanguage = "es";
-            res.set("Accept-Language", "es");
+    attachI18(Internationalization){
+        return async function (req, res, next){
+            let acceptLanguage = req.header('Accept-Language');
+            try {
+                LanguageEnum.isValidEnum(acceptLanguage);
+            } catch (error) {
+                acceptLanguage = "es";
+                res.set("Accept-Language", "es");
+            }
+            req.polyglot = new Polyglot();
+            if(acceptLanguage === LanguageEnum.EN){
+                req.polyglot.extend(Internationalization.en);
+            } else {
+                req.polyglot.extend(Internationalization.es);
+            }
+            next();
         }
-        req.polyglot = new Polyglot();
-        if(acceptLanguage === LanguageEnum.EN){
-            req.polyglot.extend(Internationalization.en);
-        } else {
-            req.polyglot.extend(Internationalization.es);
-        }
-        next();
     }
 }
 

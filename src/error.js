0edea9e5ede62e820096a9e2db11092f26812596
diego jose orellana bojorquez <diegoorellana@twowheelstogo.com>
2020-11-10 14:09:58 -0600

    fixdata

--- src/error.js
+++ src/error.js
@@ -6,9 +6,14 @@ class ErrorMiddleware{
     async checkErrors(err, req, res, next){
         if(err.code != undefined)
         {
-            let message = req.polyglot.t(err.message);
-            return res.status(err.code).send({ message });
-        }else{
+            if(err.field == "file"){
+                return res.status(409).send({ message:err.code });
+            }else{
+                let message = req.polyglot.t(err.message);
+                return res.status(err.code).send({ message });
+            }
+        }
+        else{
             console.log(err);
             return res.status(500).send({err}); 
         }
